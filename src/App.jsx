fada1202c9e2ccc7e7b9fd2e3e66e0605ce680b4
João Moreira <joaojpmoreira25@gmail.com>
2024-05-02 23:30:02 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,198 @@
+// src/Game.js
+import { useEffect } from 'react'
+import Phaser from 'phaser'
+
+const Game = () => {
+  useEffect(() => {
+    const config = {
+      type: Phaser.AUTO,
+      width: 800,
+      height: 600,
+      parent: "game-container",
+      pixelArt: true,
+      physics: {
+        default: "arcade",
+        arcade: {
+          gravity: { y: 0 }
+        }
+      },
+      scene: {
+        preload: preload,
+        create: create,
+        update: update
+      }
+    }
+
+    const game = new Phaser.Game(config)
+    let cursors
+    let player
+    let showDebug = false
+
+    function preload() {
+      this.load.image("tiles", "https://mikewesthad.github.io/phaser-3-tilemap-blog-posts/post-1/assets/tilesets/tuxmon-sample-32px-extruded.png")
+      this.load.tilemapTiledJSON("map", "https://mikewesthad.github.io/phaser-3-tilemap-blog-posts/post-1/assets/tilemaps/tuxemon-town.json")
+    
+      // An atlas is a way to pack multiple images together into one texture. I'm using it to load all
+      // the player animations (walking left, walking right, etc.) in one image. For more info see:
+      //  https://labs.phaser.io/view.html?src=src/animation/texture%20atlas%20animation.js
+      // If you don't use an atlas, you can do the same thing with a spritesheet, see:
+      //  https://labs.phaser.io/view.html?src=src/animation/single%20sprite%20sheet.js
+      this.load.atlas("atlas", "https://raw.githubusercontent.com/joaomni/cdn/main/ethan.webp", "https://raw.githubusercontent.com/joaomni/cdn/main/ethan.json")
+    }
+    
+    function create() {
+      const map = this.make.tilemap({ key: "map" })
+    
+      // Parameters are the name you gave the tileset in Tiled and then the key of the tileset image in
+      // Phaser's cache (i.e. the name you used in preload)
+      const tileset = map.addTilesetImage("tuxmon-sample-32px-extruded", "tiles")
+    
+      // Parameters: layer name (or index) from Tiled, tileset, x, y
+      const belowLayer = map.createLayer("Below Player", tileset, 0, 0)
+      const worldLayer = map.createLayer("World", tileset, 0, 0)
+      const aboveLayer = map.createLayer("Above Player", tileset, 0, 0)
+    
+      worldLayer.setCollisionByProperty({ collides: true })
+    
+      // By default, everything gets depth sorted on the screen in the order we created things. Here, we
+      // want the "Above Player" layer to sit on top of the player, so we explicitly give it a depth.
+      // Higher depths will sit on top of lower depth objects.
+      aboveLayer.setDepth(10)
+    
+      // Object layers in Tiled let you embed extra info into a map - like a spawn point or custom
+      // collision shapes. In the tmx file, there's an object layer with a point named "Spawn Point"
+      const spawnPoint = map.findObject("Objects", obj => obj.name === "Spawn Point")
+    
+      // Create a sprite with physics enabled via the physics system. The image used for the sprite has
+      // a bit of whitespace, so I'm using setSize & setOffset to control the size of the player's body.
+      player = this.physics.add
+        .sprite(spawnPoint.x, spawnPoint.y, "atlas", "ethan-front")
+        .setSize(28, 40)
+        .setOffset(3, 6)
+    
+      // Watch the player and worldLayer for collisions, for the duration of the scene:
+      this.physics.add.collider(player, worldLayer)
+    
+      // Create the player's walking animations from the texture atlas. These are stored in the global
+      // animation manager so any sprite can access them.
+      const anims = this.anims
+      anims.create({
+        key: "ethan-left-walk",
+        frames: anims.generateFrameNames("atlas", { prefix: "ethan-left-walk.", start: 0, end: 3, zeroPad: 3 }),
+        frameRate: 10,
+        repeat: -1
+      })
+      anims.create({
+        key: "ethan-right-walk",
+        frames: anims.generateFrameNames("atlas", { prefix: "ethan-right-walk.", start: 0, end: 3, zeroPad: 3 }),
+        frameRate: 10,
+        repeat: -1
+      })
+      anims.create({
+        key: "ethan-front-walk",
+        frames: anims.generateFrameNames("atlas", { prefix: "ethan-front-walk.", start: 0, end: 3, zeroPad: 3 }),
+        frameRate: 10,
+        repeat: -1
+      })
+      anims.create({
+        key: "ethan-back-walk",
+        frames: anims.generateFrameNames("atlas", { prefix: "ethan-back-walk.", start: 0, end: 3, zeroPad: 3 }),
+        frameRate: 10,
+        repeat: -1
+      })
+    
+      const camera = this.cameras.main
+      camera.startFollow(player)
+      camera.setBounds(0, 0, map.widthInPixels, map.heightInPixels)
+    
+      cursors = this.input.keyboard.createCursorKeys()
+    
+      // Help text that has a "fixed" position on the screen
+      this.add
+        .text(16, 16, 'Arrow keys to move\nPress "D" to show hitboxes', {
+          font: "18px monospace",
+          fill: "#000000",
+          padding: { x: 20, y: 10 },
+          backgroundColor: "#ffffff"
+        })
+        .setScrollFactor(0)
+        .setDepth(30)
+    
+      this.add
+      .image(16, 16, 'atlas', 1)
+      .setScrollFactor(0)
+      .setDepth(30)
+    
+      // Debug graphics
+      this.input.keyboard.once("keydown-D", event => {
+        // Turn on physics debugging to show player's hitbox
+        this.physics.world.createDebugGraphic()
+    
+        // Create worldLayer collision graphic above the player, but below the help text
+        const graphics = this.add
+          .graphics()
+          .setAlpha(0.75)
+          .setDepth(20)
+        worldLayer.renderDebug(graphics, {
+          tileColor: null, // Color of non-colliding tiles
+          collidingTileColor: new Phaser.Display.Color(243, 134, 48, 255), // Color of colliding tiles
+          faceColor: new Phaser.Display.Color(40, 39, 37, 255) // Color of colliding face edges
+        })
+      })
+    }
+    
+    function update(time, delta) {
+      const speed = 175
+      const prevVelocity = player.body.velocity.clone()
+    
+      // Stop any previous movement from the last frame
+      player.body.setVelocity(0)
+    
+      // Horizontal movement
+      if (cursors.left.isDown) {
+        player.body.setVelocityX(-speed)
+      } else if (cursors.right.isDown) {
+        player.body.setVelocityX(speed)
+      }
+    
+      // Vertical movement
+      if (cursors.up.isDown) {
+        player.body.setVelocityY(-speed)
+      } else if (cursors.down.isDown) {
+        player.body.setVelocityY(speed)
+      }
+    
+      // Normalize and scale the velocity so that player can't move faster along a diagonal
+      player.body.velocity.normalize().scale(speed)
+    
+      // Update the animation last and give left/right animations precedence over up/down animations
+      if (cursors.left.isDown) {
+        player.anims.play("ethan-left-walk", true)
+      } else if (cursors.right.isDown) {
+        player.anims.play("ethan-right-walk", true)
+      } else if (cursors.up.isDown) {
+        player.anims.play("ethan-back-walk", true)
+      } else if (cursors.down.isDown) {
+        player.anims.play("ethan-front-walk", true)
+      } else {
+        player.anims.stop()
+    
+        // If we were moving, pick and idle frame to use
+        if (prevVelocity.x < 0) player.setTexture("atlas", "ethan-left")
+        else if (prevVelocity.x > 0) player.setTexture("atlas", "ethan-right")
+        else if (prevVelocity.y < 0) player.setTexture("atlas", "ethan-back")
+        else if (prevVelocity.y > 0) player.setTexture("atlas", "ethan-front")
+      }
+    }
+
+    return () => {
+      game.destroy(true)
+    }
+  }, [])
+
+  return (
+    <div id="game-container" />
+  )
+}
+
+export default Game
